9126230b4e94d8d1d806904d8648da8a0fd7f553
giftedpromise <giftedpromise1992@gmail.com>
2024-04-24 16:41:47 -1200

    JavaScript

--- script.js
+++ script.js
@@ -456,3 +456,37 @@ number = -5;
 number > 0
   ? console.log(`${number} is a positive mumber`)
   : console.log(`${number} is a negative mumber`);
+
+/*
+  Date Object
+Time is an important thing. We like to know the time a certain activity or event. 
+In JavaScript current time and date is created using JavaScript Date Object. 
+The object we create using Date object provides many methods to work with date and time.
+The methods we use to get date and time information from a date object values are started with a word get 
+because it provide the information. 
+getFullYear(), getMonth(), getDate(), getDay(), getHours(), 
+getMinutes, getSeconds(), getMilliseconds(), getTime(), getDay()
+
+*/
+
+//Creating a time object
+
+let now = new Date();
+console.log(now);
+
+//Get year
+console.log(now.getFullYear());
+
+//Get month
+console.log(now.getMonth());
+
+//Get Day
+console.log(now.getDay());
+
+const year = now.getFullYear(); // return year
+const month = now.getMonth() + 1; // return month(0 - 11)
+const date = now.getDate(); // return date (1 - 31)
+const hours = now.getHours(); // return number (0 - 23)
+const minutes = now.getMinutes(); // return number (0 -59)
+
+console.log(`${date}/${month}/${year} ${hours}:${minutes}`); // 4/1/2020 0:56
