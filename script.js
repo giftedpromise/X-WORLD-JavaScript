bc7f49e674afdaf11257dc832f61f68528ebf890
giftedpromise <giftedpromise1992@gmail.com>
2024-04-26 17:03:39 -1200

    JavaScript

--- script.js
+++ script.js
@@ -740,3 +740,25 @@ if (yourAge > myAge) {
 } else {
   console.log("We are the same age.");
 }
+
+/* If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+
+using if else
+ternary operator.
+  let a = 4
+  let b = 3
+  4 is greater than 3
+
+  */
+let ac = 4;
+let bc = 3;
+
+if (ac > bc) {
+  console.log(`${ac} is greater than ${bc}`);
+} else {
+  console.log(`${ac} is less than ${bc}`);
+}
+
+ac > bc
+  ? console.log(`${ac} is greater than ${bc}`)
+  : console.log(`${ac} is less than ${bc}`);
