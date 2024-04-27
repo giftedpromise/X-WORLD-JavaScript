579bb858c084f3f8409b5515dbc86600bf490083
giftedpromise <giftedpromise1992@gmail.com>
2024-04-26 16:54:27 -1200

    JavaScript

--- script.js
+++ script.js
@@ -720,6 +720,13 @@ if (age2 >= 18) {
 
 //Exercise 2
 
+/* Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+
+Enter your age: 30
+You are 5 years older than me.
+
+*/
+
 let myAge = 18;
 let yourAge = prompt("Enter Your Age");
 
