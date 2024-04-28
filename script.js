335edec365fa41b603fbf888d8e4497bdfa209ee
giftedpromise <giftedpromise1992@gmail.com>
2024-04-28 20:33:06 +0100

    JavaScript

--- script.js
+++ script.js
@@ -1231,3 +1231,74 @@ console.log(fruits4); // ['banana', 'orange', 'mango', 'lemon', 'apple']
 
 fruits4.push("lime");
 console.log(fruits4); // ['banana', 'orange', 'mango', 'lemon', 'apple', 'lime']
+
+//Removing the end element using pop
+//pop: Removing item in the end.
+
+const numbers12 = [1, 2, 3, 4, 5];
+numbers12.pop(); // -> remove one item from the end
+console.log(numbers12); // -> [1,2,3,4]
+
+//Removing an element from the beginning
+//shift: Removing one array element in the beginning of the array.
+
+const numbers13 = [1, 2, 3, 4, 5];
+numbers13.shift(); // -> remove one item from the beginning
+console.log(numbers13); // -> [2,3,4,5]
+
+//Add an element from the beginning
+//unshift: Adding array element in the beginning of the array.
+
+const numbers14 = [1, 2, 3, 4, 5];
+numbers14.unshift(0); // -> add one item from the beginning
+console.log(numbers14); // -> [0,1,2,3,4,5]
+
+//Reversing array order
+//reverse: reverse the order of an array.
+
+const numbers15 = [1, 2, 3, 4, 5];
+numbers15.reverse(); // -> reverse array order
+console.log(numbers15); // [5, 4, 3, 2, 1]
+
+numbers15.reverse();
+console.log(numbers15); // [1, 2, 3, 4, 5]
+
+//Sorting elements in array
+//sort: arrange array elements in ascending order. Sort takes a call back function,
+//we will see how we use sort with a call back function in the coming sections.
+
+const webTechs5 = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+
+webTechs5.sort();
+console.log(webTechs5); // ["CSS", "HTML", "JavaScript", "MongoDB", "Node", "React", "Redux"]
+
+webTechs5.reverse(); // after sorting we can reverse it
+console.log(webTechs5); // ["Redux", "React", "Node", "MongoDB", "JavaScript", "HTML", "CSS"]
+
+//Array of arrays
+//Array can store different data types including an array itself. Let us create an array of arrays.
+
+const firstNums = [1, 2, 3];
+const secondNums = [1, 4, 9];
+
+const arrayOfArray = [
+  [1, 2, 3],
+  [1, 2, 3],
+];
+console.log(arrayOfArray[0]); // [1, 2, 3]
+
+const frontEnd = ["HTML", "CSS", "JS", "React", "Redux"];
+const backEnd = ["Node", "Express", "MongoDB"];
+const fullStack = [frontEnd, backEnd];
+console.log(fullStack); // [["HTML", "CSS", "JS", "React", "Redux"], ["Node", "Express", "MongoDB"]]
+console.log(fullStack.length); // 2
+console.log(fullStack[0]); // ["HTML", "CSS", "JS", "React", "Redux"]
+console.log(fullStack[1]); // ["Node", "Express", "MongoDB"]
