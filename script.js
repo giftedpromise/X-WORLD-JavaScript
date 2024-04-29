ae2f4a72dd7cbc7cd2b7368d890845e5c43c7cff
giftedpromise <giftedpromise1992@gmail.com>
2024-04-29 06:29:35 +0100

    JavaScript

--- script.js
+++ script.js
@@ -1183,7 +1183,7 @@ const names2 = ["Asabeneh", "Mathias", "Elias", "Brook"];
 
 console.log(names2.join()); // Asabeneh,Mathias,Elias,Brook
 console.log(names2.join("")); //AsabenehMathiasEliasBrook
-console.log(name2.join(" ")); //Asabeneh Mathias Elias Brook
+console.log(names2.join(" ")); //Asabeneh Mathias Elias Brook
 console.log(names2.join(", ")); //Asabeneh, Mathias, Elias, Brook
 console.log(names2.join(" # ")); //Asabeneh # Mathias # Elias # Brook
 
@@ -1402,7 +1402,7 @@ while (i < 10) {
 //do...while Loop:
 //This loop is similar to the while loop, but it always runs at least once, even if the condition is false at the start.
 
-let i = 0;
+var i = 0;
 do {
   console.log(i);
   i++;
@@ -1445,3 +1445,85 @@ for (const country of countries8) {
 
 //break
 //Break is used to interrupt a loop.
+for (i = 0; i < 10; i++) {
+  if (i == 3) {
+    break;
+  }
+  console.log(i);
+}
+
+//continue
+//We use the keyword continue to skip a certain iterations.
+for (let i = 0; i <= 5; i++) {
+  if (i == 3) {
+    continue;
+  }
+  console.log(i);
+}
+
+//Exercise
+/* 
+Write a loop that makes the following pattern using console.log():
+
+    #
+    ##
+    ###
+    ####
+    #####
+    ######
+    #######
+
+    */
+
+const numberOfRows = 7;
+
+for (let i = 1; i <= numberOfRows; i++) {
+  let pattern = "#".repeat(i);
+  console.log(pattern);
+}
+
+/* 
+Use loop to print the following pattern:
+
+0 x 0 = 0
+1 x 1 = 1
+2 x 2 = 4
+3 x 3 = 9
+4 x 4 = 16
+5 x 5 = 25
+6 x 6 = 36
+7 x 7 = 49
+8 x 8 = 64
+9 x 9 = 81
+10 x 10 = 100
+
+*/
+
+for (let i = 0; i <= 10; i++) {
+  console.log(`${i} * ${i} = ${i * i}`);
+}
+
+/* 
+Using loop print the following pattern
+
+ i    i^2   i^3
+ 0    0     0
+ 1    1     1
+ 2    4     8
+ 3    9     27
+ 4    16    64
+ 5    25    125
+ 6    36    216
+ 7    49    343
+ 8    64    512
+ 9    81    729
+ 10   100   1000
+
+ */
+
+for (let i = 0; i <= 10; i++) {
+  const iSquared = i * i;
+  const iCubed = i * i * i;
+
+  console.log(`${i}  ${iSquared} ${iCubed}`);
+}
