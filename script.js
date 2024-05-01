2c246266f457ae55764f0da8a594bac963e25178
giftedpromise <giftedpromise1992@gmail.com>
2024-04-30 15:32:28 -1200

    JavaScript

--- script.js
+++ script.js
@@ -1624,3 +1624,33 @@ function sumTwoNumbers(numOne, numTwo) {
 }
 sumTwoNumbers(10, 20); // calling functions
 // If a function doesn't return it doesn't store data, so it should return
+
+function printFullName(firstName, LastName) {
+  return `${firstName} ${lastName}`;
+}
+
+console.log(printFullName("Asabeneh", "Yetayeh"));
+
+//Function with unlimited number of parameters
+//Sometimes we do not know how many arguments the user going to pass.
+//Therefore, we should know how to write a function which can take unlimited number of arguments.
+// Let us access the arguments object
+
+function sumAllNums() {
+  console.log(arguments);
+}
+
+sumAllNums(1, 2, 3, 4);
+// Arguments(4) [1, 2, 3, 4, callee: ƒ, Symbol(Symbol.iterator): ƒ]
+
+function sumAllNums() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+
+console.log(sumAllNums(1, 2, 3, 4)); // 10
+console.log(sumAllNums(10, 20, 13, 40, 10)); // 93
+console.log(sumAllNums(15, 20, 30, 25, 10, 33, 40)); // 173
