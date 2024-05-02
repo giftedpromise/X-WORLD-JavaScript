073a2652f56df4dbd2639dc5630e1d538a7f38e0
giftedpromise <giftedpromise1992@gmail.com>
2024-05-02 15:08:08 +0100

    JavaScript

--- script.js
+++ script.js
@@ -1678,6 +1678,8 @@ console.log(sumAllNums1(10, 20, 13, 40, 10)); // 93
 console.log(sumAllNums1(15, 20, 30, 25, 10, 33, 40)); // 173
 
 //Anonymous function
+//An anonymous function is not accessible after its initial creation.
+// Therefore, you often need to assign it to a variable.
 const anonymousFun = function () {
   console.log(
     "I am an anonymous function and my value is stored in anonymousFun"
@@ -1783,6 +1785,18 @@ function showMe() {
 //Function hoisting is a mechanism which the JavaScript engine physically moves
 // function declarations to the top of the code before executing them.
 
+//Introduction to the JavaScript recursive functions
+//A recursive function is a function that calls itself until it doesn’t.
+//This technique is called recursion.
+function recurse() {
+  if (condition) {
+    // stop calling itself
+    //...
+  } else {
+    recurse();
+  }
+}
+
 //Exercises: Level 1
 //Declare a function fullName and it print out your full name.
 //Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
@@ -1865,7 +1879,20 @@ function exampleFunction() {
 //so an object is a key value pair.
 //Creating an empty object
 //An empty object
+//Introduction to the JavaScript object methods
+//An object is a collection of key/value pairs or properties.
+//When the value is a function, the property becomes a method.
+//Typically, you use methods to describe the object’s behaviors.
+let person5 = {
+  firstName: "John",
+  lastName: "Doe",
+};
 
+person5.greet = function () {
+  console.log("Hello");
+};
+
+person5.greet();
 const person = {};
 
 //Creating an objecting with values
@@ -2023,3 +2050,73 @@ dog.getDogInfo = function () {
     this.legs
   } legs. When it barks, it goes ${this.bark()}.`;
 };
+
+//DAY 8
+//TOPIC COVERED: Higher Order Function
+//Higher order functions are functions which take other function as a parameter or return a function as a value.
+// The function passed as a parameter is called callback.
+// a callback function, the name of the function could be any name
+const callback = (n) => {
+  return n ** 2;
+};
+
+// function that takes other function as a callback
+function cube(callback, n) {
+  return callback(n) * n;
+}
+
+console.log(cube(callback, 3));
+
+//Returning function
+//Higher order functions return function as a value​
+
+// Higher order function returning an other function
+const higherOrder = (n) => {
+  const doSomething = (m) => {
+    const doWhatEver = (t) => {
+      return 2 * n + 3 * m + t;
+    };
+    return doWhatEver;
+  };
+  return doSomething;
+};
+console.log(higherOrder(2)(3)(10));
+
+/* Setting time
+In JavaScript we can execute some activities in a certain interval of time or we can schedule(wait) for some time to execute some activities.
+
+setInterval
+setTimeout
+Setting Interval using a setInterval function
+In JavaScript, we use setInterval higher order function to do some activity continuously with in some interval of time. The setInterval global method take a callback function and a duration as a parameter. 
+The duration is in milliseconds and the callback will be always called in that interval of time.
+
+*/
+function sayHello() {
+  console.log("Hello");
+}
+setInterval(sayHello, 1000); // it prints hello in every second, 1000ms is 1s
+
+/*Setting a time using a setTimeout
+In JavaScript, we use setTimeout higher order function
+ to execute some action at some time in the future.
+  The setTimeout global method take a callback function and a duration as a parameter. 
+The duration is in milliseconds and the callback wait for that amount of time.
+*/
+function sayHell1o() {
+  console.log("Hello");
+}
+setTimeout(sayHello, 2000); // it prints hello after it waits for 2 seconds.
+
+/*
+Functional Programming
+Instead of writing regular loop, latest version of JavaScript introduced 
+lots of built in methods which can help us to solve complicated problems. 
+All builtin methods take callback function. In this section, we will see forEach, 
+map, filter, reduce, find, every, some, and sort.
+
+forEach
+forEach: Iterate an array elements. We use forEach only with arrays. 
+It takes a callback function with elements, index parameter and array itself. 
+The index and the array optional.
+*/
