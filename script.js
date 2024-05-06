ef1d198259f8bfe3a0a80874cf65b885c3037092
giftedpromise <giftedpromise1992@gmail.com>
2024-05-05 21:53:19 -1200

    JavaScript

--- script.js
+++ script.js
@@ -2663,3 +2663,16 @@ const wholeNumbers = [...evens, ...odds];
 console.log(evenNumbers);
 console.log(oddNumbers);
 console.log(wholeNumbers);
+
+//Spread operator to copy object
+//We can copy an object using a spread operator
+
+const user = {
+  name: "Asabeneh",
+  title: "Programmer",
+  country: "Finland",
+  city: "Helsinki",
+};
+
+const copiedUser = { ...user };
+console.log(copiedUser);
