86207d689e3108c9219a2c786532825f2c7d214f
giftedpromise <giftedpromise1992@gmail.com>
2024-05-14 00:16:41 -1200

    JavaScript

--- script.js
+++ script.js
@@ -3684,3 +3684,42 @@ btn.addEventListener("click", showAlert);
 
 // remove the event listener
 btn.removeEventListener("click", showAlert);
+
+//JavaScript Page Load Events
+
+/* When you open a page, the following events occur in sequence:
+
+DOMContentLoaded – the browser fully loaded HTML and completed building the DOM tree.
+ However, it hasn’t loaded external resources like stylesheets and images. 
+ In this event, you can start selecting DOM nodes or initialize the interface.
+load – the browser fully loaded the HTML and also external resources like images and stylesheets.
+When you leave the page, the following events fire in sequence:
+
+beforeunload – fires before the page and resources are unloaded. 
+You can use this event to show a confirmation dialog to confirm if you really want to leave the page. 
+By doing this, you can prevent data loss in case you are filling out a form 
+and accidentally click a link to navigate to another page.
+unload – fires when the page has completely unloaded. 
+You can use this event to send the analytic data or to clean up resources.
+*/
+addEventListener("DOMContentLoaded", (event) => {
+  console.log("The DOM is fully loaded.");
+});
+
+addEventListener("load", (event) => {
+  console.log("The page is fully loaded.");
+});
+
+addEventListener("beforeunload", (event) => {
+  // show the confirmation dialog
+  event.preventDefault();
+  // Google Chrome requires returnValue to be set.
+  event.returnValue = "";
+});
+
+addEventListener("unload", (event) => {
+  // send analytic data
+});
+
+//Day 24
+//TOPIC COVERED: JavaScript Form
