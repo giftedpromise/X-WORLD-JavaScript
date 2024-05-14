360fe622b34767aa62861315b4da0e1428e7f19a
giftedpromise <giftedpromise1992@gmail.com>
2024-05-13 23:26:14 -1200

    JavaScript

--- script.js
+++ script.js
@@ -3638,3 +3638,49 @@ btn.onClick = function () {
 
 //To remove the event handler, you set the value of the event handler property to null:
 btn.onclick = null;
+
+/* 3) DOM Level 2 event handlers
+DOM Level 2 Event Handlers provide two main methods for dealing
+ with the registering/deregistering event listeners:
+
+addEventListener() – register an event handler
+removeEventListener() – remove an event handler
+These methods are available in all DOM nodes.
+
+The addEventListener() method
+The addEventListener() method accepts three arguments: an event name, an event handler function, 
+and a Boolean value that instructs the method to call the event handler during 
+the capture phase (true) or during the bubble phase (false). For example:
+*/
+//let btn = document.querySelector('#btn');
+btn.addEventListener("click", function (event) {
+  alert(event.type); // click
+});
+
+/* It is possible to add multiple event handlers to handle a single event, like this:
+
+let btn = document.querySelector('#btn');
+btn.addEventListener('click',function(event) {
+    alert(event.type); // click
+});
+
+btn.addEventListener('click',function(event) {
+    alert('Clicked!');
+});
+
+
+The removeEventListener() method
+The removeEventListener() removes an event listener that was added via the addEventListener(). However, 
+you need to pass the same arguments as were passed to the addEventListener(). For example:
+*/
+
+//let btn = document.querySelector("#btn");
+
+// add the event listener
+let showAlert = function () {
+  alert("Clicked!");
+};
+btn.addEventListener("click", showAlert);
+
+// remove the event listener
+btn.removeEventListener("click", showAlert);
