7c534d887503aef5ba58fe42ce541b457126d789
giftedpromise <giftedpromise1992@gmail.com>
2024-05-16 23:23:02 -1200

    JavaScript

--- script.js
+++ script.js
@@ -3801,3 +3801,61 @@ const people = [
   "Beecher, Henry",
   "Biondo, Frank",
 ];
+
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+const bornIn1500 = inventors.filter(
+  (inventor) => inventor.year >= 1500 && inventor.year < 1600
+);
+
+console.table(bornIn1500);
+// Array.prototype.map()
+// 2. Give us an array of the inventors first and last names
+const lastFirstName = inventors.map(
+  (inventor) => `${inventor.first} ${inventor.last}`
+);
+
+console.log(lastFirstName);
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+const ordered = inventors.sort(function (a, b) {
+  if (a.year > b.year) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+console.log(ordered);
+
+const orderd1 = inventors.sort((a, b) => (a.year > B.year ? 1 : -1));
+console.log(orderd1);
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live all together?
+
+// 5. Sort the inventors by years lived
+
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+const data = [
+  "car",
+  "car",
+  "truck",
+  "truck",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "car",
+  "truck",
+];
