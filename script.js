d2f9167d68dd90401f53c6ce503227ec732ed0ac
giftedpromise <giftedpromise1992@gmail.com>
2024-05-17 12:39:30 -1200

    JavaScript

--- script.js
+++ script.js
@@ -3913,3 +3913,12 @@ console.log(index1);
 // comments.splice(index, 1);
 
 const newComments = [...comments.slice(0, index), ...comments.slice(index + 1)];
+
+const getUserInfo = (firstName, lastName, country, title, skills) => {
+  return `${firstName} ${lastName} a ${title} developer based in${country}. He knows ${skills.join()}`;
+};
+
+const skills = ["HTML", "CSS", "JS", "React"];
+console.log(
+  getUserInfo("Asabeneh", "Yetayeh", "Finland", "FullStack Developer", skills)
+);
